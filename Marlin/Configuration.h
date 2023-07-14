0195ee9770b32132cedd0c2452efd4df11461f3e
BotchedRPR <thenxguy0@gmail.com>
2023-07-14 21:12:12 +0200

    Make it known that it's an alpha

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(BotchedRPR, marlin-fm 1.0.0)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(marlin-fm 1.0.0 ALPHA 1)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
